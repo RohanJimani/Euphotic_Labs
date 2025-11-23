dfcd68752f0740ec8ee3f5086ce1433614dccf48
RohanJimani <133959914+RohanJimani@users.noreply.github.com>
2025-11-23 10:21:02 -0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api", 
+  baseURL: "https://euphotic-labs.onrender.com/api", 
 });
 
 export const getDishes = () => API.get("/dishes");
